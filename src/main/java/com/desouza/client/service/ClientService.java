1dc046d68603f7d95d41080759b7a0e7e6cf7ca6
ozysouza <ozysouzaa@gmail.com>
2025-06-03 23:46:09 -0400

    Create ClientService, Add method to return all Clients on pages

---
+++ src/main/java/com/desouza/client/service/ClientService.java
@@ -0,0 +1,25 @@
+package com.desouza.client.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.desouza.client.dto.ClientDTO;
+import com.desouza.client.entity.Client;
+import com.desouza.client.repository.ClientRepository;
+
+@Service
+public class ClientService {
+
+    @Autowired
+    private ClientRepository repository;
+
+    @Transactional
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
+    }
+
+}
