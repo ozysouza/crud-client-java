f5a41304711b1e20eaed32458241109ead3a2711
ozysouza <ozysouzaa@gmail.com>
2025-06-04 12:18:52 -0400

    Create method to handle MethodArgumentNotValidException

--- src/main/java/com/desouza/client/controllers/handlers/ControllerExceptionHandler.java
+++ src/main/java/com/desouza/client/controllers/handlers/ControllerExceptionHandler.java
@@ -4,10 +4,13 @@ import java.time.Instant;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import com.desouza.client.dto.error.CustomError;
+import com.desouza.client.dto.error.ValidationError;
 import com.desouza.client.service.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -19,6 +22,19 @@ public class ControllerExceptionHandler {
     public ResponseEntity<CustomError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
         HttpStatus status = HttpStatus.NOT_FOUND;
         CustomError error = new CustomError(e.getMessage(), status.value(), request.getRequestURI(), Instant.now());
+        return ResponseEntity.status(status).body(error);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<CustomError> methodArgumentNotValidException(MethodArgumentNotValidException e,
+            HttpServletRequest request) {
+        HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+        ValidationError error = new ValidationError("Invalid data", status.value(), request.getRequestURI(),
+                Instant.now());
+
+        for (FieldError fieldError : e.getBindingResult().getFieldErrors()) {
+            error.addError(fieldError.getField(), fieldError.getDefaultMessage());
+        }
         return ResponseEntity.status(status).body(error);
     }
 
