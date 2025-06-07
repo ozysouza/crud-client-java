5640fe9b0d2274f5bb6cf3e98b7c5c349e576f21
ozysouza <ozysouzaa@gmail.com>
2025-06-07 03:04:33 -0400

    Update seed

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -25,3 +25,18 @@ INSERT INTO tb_person (name, salary, department_id) VALUES ('James', 4000.0, 1);
 INSERT INTO tb_person (name, salary, department_id) VALUES ('Maria', 6000.0, 2);
 INSERT INTO tb_person (name, salary, department_id) VALUES ('Bob', 3000.0, 2);
 
+INSERT INTO tb_category (name) VALUES ('Books');
+INSERT INTO tb_category (name) VALUES ('Eletronic');
+INSERT INTO tb_category (name) VALUES ('Computers');
+
+INSERT INTO tb_product (name, price) VALUES ('The Lord of the Rings', 90.5);
+INSERT INTO tb_product (name, price) VALUES ('Smart TV', 2190.0);
+INSERT INTO tb_product (name, price) VALUES ('Macbook Pro', 1250.0);
+INSERT INTO tb_product (name, price) VALUES ('PC Gamer', 1200.0);
+
+INSERT INTO tb_product_category (product_id, category_id) VALUES (1, 1);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (2, 2);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (2, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (3, 3);
+INSERT INTO tb_product_category (product_id, category_id) VALUES (4, 3);
+
